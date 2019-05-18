0fd4d19c236487dd09afe78b7789e750d136464c
Samuel Mann <samuelcmann@gmail.com>
2019-05-18 19:46:13 +0200

    add sign out

--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import  { Link } from 'react-router-dom'
 import * as ROUTES from '../../constants/routes'
+import SignOutButton from '../SignOut'
 
 const Navigation = () => (
-    <div>
-        <ul>
-            <li>
-                <Link to={ROUTES.SIGN_IN}>Sign in</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.SIGN_UP}>Sign up</Link>
-            </li>
-            <li>
-                <Link to={ROUTES.PROFILE}>Profile</Link>
-            </li>
-        </ul>
-    </div>
+  <div>
+      <ul>
+          <li>
+              <Link to={ROUTES.SIGN_IN}>Sign in</Link>
+          </li>
+          <li>
+              <Link to={ROUTES.SIGN_UP}>Sign up</Link>
+          </li>
+          <li>
+              <Link to={ROUTES.PROFILE}>Profile</Link>
+          </li>
+          <li>
+              <SignOutButton/>
+          </li>
+      </ul>
+  </div>
+
 )
 
 export default Navigation
